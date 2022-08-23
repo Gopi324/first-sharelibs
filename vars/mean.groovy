a7aee2a54a92a1c8ba0907abca2da9eb39dba466
Gopi324 <109789706+Gopi324@users.noreply.github.com>
2022-08-23 23:22:08 +0530

    Update mean.groovy

--- vars/mean.groovy
+++ vars/mean.groovy
@@ -1,6 +1,6 @@
 def call(ip,credId,user){
   sshagent([credId]) {
-     // copy war
+    // copy war
     sh 'scp -o StrictHostKeyChecking=no target/myweb-0.0.9.war ${user}@${ip}:/opt/tom9/webapps'
     // stop tomcat
     sh 'ssh ${user}@${ip} /opt/tom9/bin/shutdown.sh'
